fecd9fa966b97e6e5f439c32c98f2d3862fb839d
MattFreitas-Dev <italomateusf1@gmail.com>
2025-02-17 15:06:29 -0300

    Main Menu changes

--- Assets/Scripts/LevelManagement/LevelManager.cs
+++ Assets/Scripts/LevelManagement/LevelManager.cs
@@ -13,22 +13,38 @@ public class LevelManager : MonoBehaviour
 {
     public AudioMixer audioMixer;
     public TMP_Dropdown resDropdown;
+
     Resolution[] resolutions;
 
 	private void Start()
 	{
         resolutions = Screen.resolutions;
-        resDropdown.ClearOptions();
+		resDropdown.ClearOptions();
 
         List<string> options = new List<string>();
+
+        int currentResIndex = 0;
         for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + "x" + resolutions[i].height;
             options.Add(option);
+
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResIndex = i;
+            }
         }
 
         resDropdown.AddOptions(options);
+        resDropdown.value = currentResIndex;
+        resDropdown.RefreshShownValue();
 	}
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
 
 	public void StartGame()
     {
@@ -42,7 +58,7 @@ public class LevelManager : MonoBehaviour
 
     public void SetVolume(float volume)
     {
-        audioMixer.SetFloat("volume", volume);
+        audioMixer.SetFloat("volume", Mathf.Log10(volume)*20);
     }
 
     public void SetQuality(int qualityIndex)
