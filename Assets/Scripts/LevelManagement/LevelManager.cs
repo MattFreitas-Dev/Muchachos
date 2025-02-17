67bc6b221fbf568bed44f0db9c54be0948240c52
MattFreitas-Dev <italomateusf1@gmail.com>
2025-02-17 13:42:24 -0300

    Menu and settings

--- Assets/Scripts/LevelManagement/LevelManager.cs
+++ Assets/Scripts/LevelManagement/LevelManager.cs
@@ -2,11 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using UnityEditor.IMGUI.Controls;
+using TMPro.EditorUtilities;
+using TMPro;
 
 public class LevelManager : MonoBehaviour
 {
-    public void StartGame()
+    public AudioMixer audioMixer;
+    public TMP_Dropdown resDropdown;
+    Resolution[] resolutions;
+
+	private void Start()
+	{
+        resolutions = Screen.resolutions;
+        resDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+        }
+
+        resDropdown.AddOptions(options);
+	}
+
+	public void StartGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool fullscreen)
+    {
+        Screen.fullScreen = fullscreen;
     }
 }
